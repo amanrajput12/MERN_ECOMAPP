36d7ddbaedd3ef17f30257b656ee8f64baf56045
Aman Rajput <rajputaman724857@gmail.com>
2024-06-23 19:41:39 +0530

    origin change

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ import { PaymentRouter } from "./Router/PaymentRouter.js";
 const app = express();
 const port = process.env.PORT ||8000 ; // 
 dotenv.config();
-app.use(cors({
-  origin: 'https://onlineecomapp.netlify.app', // Replace with your frontend's domain
-  credentials: true
-}));
+app.use(cors());
 console.log(process.env.key_Id);
 app.use(express.json());
 app.use(express.static('Public'))
