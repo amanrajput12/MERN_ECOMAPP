c48aff62f1ef3de960284ec423935dc93ff2d608
Aman Rajput <rajputaman724857@gmail.com>
2024-06-23 18:09:59 +0530

    mongodb Uri update

--- Db/database.js
+++ Db/database.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async()=>{
     try {
+        console.log("mongodb uri",process.env.MONGODB_URI);
         const db = await mongoose.connect(process.env.MONGODB_URI)
         console.log("Datbase connection sucessfully");
     } catch (error) {
