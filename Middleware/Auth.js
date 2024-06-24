18bc3225070d6372a072645222ce60f82dae1f3d
Aman Rajput <rajputaman724857@gmail.com>
2024-06-24 14:05:21 +0530

    token error solve

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -1,7 +1,7 @@
  import jwt from "jsonwebtoken"
  export const verifyJwt = async function(req,res,next){
      try {
-        const token = req?.cookies?.token || req.header.token
+        const token = req?.cookies?.token || req.headers.token
 
      
 
