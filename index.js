852dd9e6e66d5188d446a41e9d4bc81c4bbb1004
Aman Rajput <rajputaman724857@gmail.com>
2024-07-01 18:48:11 +0530

    change cors for shopping application

--- index.js
+++ index.js
@@ -17,7 +17,10 @@ import { verifyJwt } from "./Middleware/Auth.js";
 const app = express();
 const port = process.env.PORT ||8000 ; // 
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin: 'https://shoppingwebapplication.vercel.app',
+  credentials: true
+}));
 console.log(process.env.key_Id);
 app.use(express.json());
 app.use(express.static('Public'))
